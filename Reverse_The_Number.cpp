e1a54d164690e1e21758ea324761670fa24258cd
Riv3tz <ruchirdude@gmail.com>
2021-01-21 15:47:41 +0530

    r

---
+++ Reverse_The_Number.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t, i;
+    cin >> t;
+    while(t--) {
+        int n, r = 0;
+        cin >> n;
+        while(n>0) {
+            r = r*10 + n%10;
+            n /= 10;
+        }
+        cout << r << endl;
+    }
+}
+
+// Author - Rivetz
