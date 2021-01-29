5d82d8ce073afca76a1d45f893e22c7c85bfb0c1
Riv3tz <ruchirdude@gmail.com>
2021-01-29 19:49:10 +0530

    later

---
+++ Lapindromes.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int f = 0;
+    int t, i;
+    cin >> t;
+    while(t--) {
+        string s;
+        cin >> s;
+        int len = s.length();
+        string sub1 = s.substr(0, len/2);
+        string sub2 = s.substr(len/2 + len%2, s.length()-1);
+        
+    }
+}
+
+// Author - Rivetz
