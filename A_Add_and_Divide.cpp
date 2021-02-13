29899fbec204c2d744caec855e179559d98cab89
Riv3tz <ruchirdude@gmail.com>
2021-02-13 08:05:04 +0530

    solved

--- A_Add_and_Divide.cpp
+++ A_Add_and_Divide.cpp
@@ -5,18 +5,18 @@ int main() {
     int t, i;
     cin >> t;
     while(t--) {
-        int ans = 1e5,c=0,a, b;
+        long long int ans = 1e18,c=0,a, b;
         cin >> a >> b;
-        for (int i = 0; i < sqrt(a); i++)
-        {
-            int t = a;
-            while(t) {
-                t = t / (b+i);
+        for(i = 0; i*i <= a; i++) {
+            if(b==1 && i==0) continue;
+            c = i;
+            long long int tmp = a;
+            while(tmp){
+                tmp = tmp / (b+i);
                 c++;
             }
-            ans = min(ans, c); 
+            ans = min(ans, c);
         }
-        
         cout << ans << endl;
     }
 }
