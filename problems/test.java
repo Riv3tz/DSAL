a0a78df1cbc3d2b51325dd4c14155f697c2329f9
Ruchir <ruchirdude@gmail.com>
2021-10-07 11:01:22 +0530

    tests

---
+++ problems/test.java
@@ -0,0 +1,17 @@
+package problems;
+
+
+/**
+ * test
+ */
+public class test {
+
+    public static void main(String[] args) {
+        System.out.print("hello");
+        fun(9);
+    }
+
+    static void fun(int a){
+        System.out.println(a);
+    }
+}
